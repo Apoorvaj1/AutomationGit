0ab924f7086ef0091b94097e365aefe16831815b
Apoorv <apoorv127@gmail.com>
2024-12-10 11:22:15 +0530

    10 Dec

---
+++ src/test/java/org/selenium/aj34/happyFlow1.java
@@ -0,0 +1,17 @@
+package org.selenium.aj34;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class happyFlow1 {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        System.out.println(driver.getTitle());
+        System.out.println("Hello World");
+        System.out.print("Apoorv");
+        System.out.println("HELLO");
+    }
+}
